70c3a1a60bdad4cec5f7190e38c89fc42062df67
merymegu <meryama.guer@gmail.com>
2023-10-29 10:10:13 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
- * _print_rev_recursion .
+ * _print_rev_recursion - print string in reverse
  * @s: The printed  string  .
+ * Return: Voidu
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s)
+	if (*s > '0')
 	{
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
