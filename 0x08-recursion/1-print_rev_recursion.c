ae932ab1ba1b80773ca075b7571e6b50187f337e
merymegu <meryama.guer@gmail.com>
2023-10-29 08:49:18 -0700

    task1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,12 +1,8 @@
 #include "main.h"
-
 /**
-  * _print_rev_recursion - main function
-  *
-  * @s: Function parameter
-  *
-  * Return: Voidu
-  */
+ * _print_rev_recursion .
+ * @s: The printed  string  .
+ */
 void _print_rev_recursion(char *s)
 {
 if (*s)
