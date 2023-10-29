85b76133ca0e88257bb5283ab886527da2614a40
merymegu <meryama.guer@gmail.com>
2023-10-29 10:47:00 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,27 +1,24 @@
 #include "main.h"
 /**
   * _pow_recursion - function name
-  * @x: base  1
-  * @y: exposant  2
-  * Return: value of a exposant
+  * @x: the value
+  * @y: the times
+  *
+  * Return: the value moltiped by times 
   */
 
 
 int _pow_recursion(int x, int y)
 {
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+	
+}	
 
-if (y < 0)
-        return (-1);
 
-else if (y == 0)
-        return (1);
-else if (y==1)
-	return (x);
-else
-        return (x * _pow_recursion(x, (y - 1)));
-
-return (0);
-}
 
 
 
