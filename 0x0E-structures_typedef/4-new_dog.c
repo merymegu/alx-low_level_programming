427363f6bcf70f048cdfa175173e5ca4db3c9e56
merymegu <meryama.guer@gmail.com>
2024-01-22 16:39:41 -0800

    task4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,52 +1,50 @@
-#include <stdlib.h>
-#include "dog.h"
-
-
 /**
-* new_dog - creates a new dog
-* @name: name of dog
-* @age: age of dog
-* @owner: owner of dog
-*
-* Return: pointer to new dog
-*/
+ * new_dog - create new dog
+ *@name:char
+ *@age:float
+ *@owner: char
+ * Return: new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-   unsigned int nl, ol, i;
-   dog_t *dog;
 
+	char *name2;
+	char *owner2;
+	int n, o, i, j;
+	struct dog *luna;
 
-   if (name == NULL || owner == NULL)
-       return (NULL);
-   dog = malloc(sizeof(dog_t));
-   if (dog == NULL)
-       return (NULL);
-   for (nl = 0; name[nl]; nl++)
-       ;
-   nl++;
-   dog->name = malloc(nl * sizeof(char));
-   if (dog->name == NULL)
-   {
-       free(dog);
-       return (NULL);
-   }
-   for (i = 0; i < nl; i++)
-       dog->name[i] = name[i];
-   dog->age = age;
-   for (ol = 0; owner[ol]; ol++)
-       ;
-   ol++;
-   dog->owner = malloc(ol * sizeof(char));
-   if (dog->owner == NULL)
-   {
-       free(dog->name);
-       free(dog);
-       return (NULL);
-   }
-   for (i = 0; i < ol; i++)
-       dog->owner[i] = owner[i];
-   return (dog);
+	luna = malloc(sizeof(struct dog));
+	if (luna == NULL)
+		return (NULL);
+	for (n = 0; *(name + n) != '\0'; n++)
+		;
+	for (o = 0; *(owner + o) != '\0'; o++)
+		;
+	name2 = malloc(n + 1);
+	if (name2 != NULL)
+	{
+		for (i = 0; i <= n; i++)
+			*(name2 + i) = *(name + i);
+	} else
+	{
+		free(luna);
+		return (NULL);
+	}
+	owner2 = malloc(o + 1);
+	if (owner2 != NULL)
+	{
+		for (j = 0; j <= o; j++)
+			*(owner2 + j) = *(owner + j);
+	} else
+	{
+		free(name2);
+		free(luna);
+		return (NULL);
+	}
+	luna->name = name2;
+	luna->age = age;
+	luna->owner = owner2;
+	return (luna);
+
 }
-
-
 
