8a0ea9b6db61f408c39563dfdd45839a02ee3931
merymegu <meryama.guer@gmail.com>
2024-01-24 11:01:46 -0800

    task0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,14 @@
 #include "function_pointers.h"
 
 /**
-*print_name - prints a name
-*@name:name to print
-*@f: function 
-*/
-
+ * print_name - Print a name
+ *@name:name
+ *@f:poniter to function which print a name
+ *
+ * Return: no return
+ */
 void print_name(char *name, void (*f)(char *))
 {
-
-if (name == NULL || f == NULL)
-return;
-
-f(name);
-
+	if (f != NULL)
+		f(name);
 }
