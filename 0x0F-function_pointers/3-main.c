0d90da16b9876677039467f2ea9bd019df364ac7
merymegu <meryama.guer@gmail.com>
2024-01-24 12:26:16 -0800

    task4

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,46 +1,36 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "3-calc.h"
-#include "function_pointers.h"
-
 /**
- * main - function
- *@argc: number of arguments
- *@argv: array with arguments
- *
- * Return: no return
- */
+ * main - Entry point
+ * @argc: the number of the parameters
+ * @argv: the parameters in the case the number to be calculated.
+(* a blank line
+* Description: this program is the enttry point for a calculator)?
+(* section header: 3-calc.h)*
+* Return: 0 in success
+*/
 int main(int argc, char *argv[])
 {
-	int n1;
-	int n2;
-	int result;
+	int n1, n2, result;
+	int (*p)(int, int);
 
-	n1 = 0;
-	n2 = 0;
-	if (argc != 4)
+	if (argc < 4 || argc > 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
-	    && argv[2][0] != '/' && argv[2][0] != '%')
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+
+	p = get_op_func(argv[2]);
+
+	if (p == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if (argv[2][1] == '\0')
-	{
-		n1 = atoi(argv[1]);
-		n2 = atoi(argv[3]);
-		result = (*get_op_func(argv[2]))(n1, n2);
-		printf("%d\n", result);
+	result = p(n1, n2);
 
-	} else
-	{
-
-		printf("Error\n");
-		exit(99);
-	}
+	printf("%d\n", result);
 	return (0);
 }
