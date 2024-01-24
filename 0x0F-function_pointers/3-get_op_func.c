c9df7938fc72428f5bee1f3fd7b231210c8cd977
merymegu <meryama.guer@gmail.com>
2024-01-24 12:20:04 -0800

    task30

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,36 +1,30 @@
+#include <stdio.h>
 #include "3-calc.h"
-#include <stdlib.h>
-#include <string.h>
-
 
 /**
-* get_op_func - ...
-* @s: ...
-*
-* Return: ...
-*/
-int (*get_op_func(char *s))(int, int)
+ * get_op_func - searchs for an integer
+ *@s:string
+ *
+ * Return: no return
+ */
+int (*get_op_func(char *s))(int a, int b)
 {
-   op_t ops[] = {
-       {"+", op_add},
-       {"-", op_sub},
-       {"*", op_mul},
-       {"/", op_div},
-       {"%", op_mod},
-       {NULL, NULL}};
-   int i = 0;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-
-   while (i < 5)
-   {
-       if (strcmp(s, ops[i].op) == 0)
-           return (ops[i].f);
-
-
-       i++;
-   }
-
-
-   return (0);
+	i = 0;
+	while (i < 5)
+	{
+		if (s[0] == ops[i].op[0])
+			return ((ops[i].f));
+		i++;
+	}
+	return (NULL);
 }
-
