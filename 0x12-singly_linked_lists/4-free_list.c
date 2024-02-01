07da156abb44d16202f2c69627f4a9674df17049
merymegu <meryama.guer@gmail.com>
2024-02-01 12:40:33 -0800

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: first element of the list
+ * Return: no return
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
+
