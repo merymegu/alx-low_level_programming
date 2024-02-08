fcdc5c6fdaaa1031141f88d66e12696fc52fa8b4
merymegu <meryama.guer@gmail.com>
2024-02-08 14:47:15 -0800

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int num  = 0;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	while (b[i + 1])
 	{
-		if (b[i] < '0' || b[i] > '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		num  = 2 * num  + (b[i] - '0');
+		i++;
 	}
 
-	return (num);
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
 
 
