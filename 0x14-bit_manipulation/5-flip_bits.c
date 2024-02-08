861dc8de74306a4496c2962cbfde8606884a118d
merymegu <meryama.guer@gmail.com>
2024-02-08 15:05:06 -0800

    task5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*flip_bits - gets the number of bits to flip to get from n to m
+*@n: initial number
+*@m: final number
+*
+*Return: the number of flipped bits
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+
+	while (n != 0 || m != 0)
+	{
+		if ((n & 1) != (m & 1))
+			count++;
+		m = m >> 1;
+		n = n >> 1;
+	}
+
+	return (count);
+
+}
