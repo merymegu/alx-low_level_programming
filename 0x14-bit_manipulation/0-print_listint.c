012fbbcf20e391f1c0ba677105c7cecf7286511d
merymegu <meryama.guer@gmail.com>
2024-02-08 14:19:09 -0800

    task0

--- 0x14-bit_manipulation/0-print_listint.c
+++ 0x14-bit_manipulation/0-print_listint.c
@@ -7,23 +7,28 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0, mult = 1;
-	int len;
+	unsigned int result, base;
+	int i;
 
-	if (b == '\0')
+	result = 0;
+	i = 0;
+	base = 1;
+
+	if (!b)
 		return (0);
 
-	for (len = 0; b[len];)
-		len++;
-
-	for (len -= 1; len >= 0; len--)
+	while (*(b + i))
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (*(b + i) != '0' && *(b + i) != '1')
 			return (0);
-
-		num += (b[len] - '0') * mult;
-		mult *= 2;
+		i++;
+	}
+	for (i--; i >= 0; i--)
+	{
+		if (*(b + i) == '1')
+			result = result + base;
+		base = base * 2;
 	}
 
-	return (num);
+	return (result);
 }
