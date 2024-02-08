92faa3a452065e088a4f491f301d9af4bf32a2e7
merymegu <meryama.guer@gmail.com>
2024-02-08 14:44:46 -0800

    task0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,30 +3,27 @@
 /**
  * print_binary - Prints the binary representation of a number.
  * @n: The number to be printed in binary.
+ *
+ *  Return: the converted number.
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-	int base = 1, i = 0;
+	int i;
+	unsigned int num  = 0;
 
-	if (b == NULL)
+	if (!b)
 		return (0);
 
-	while (b[i + 1])
+	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		i++;
+		num  = 2 * num  + (b[i] - '0');
 	}
-
-	while (i >= 0)
-	{
-		num += ((b[i] - '0') * base);
-		base *= 2;
-		i--;
-	}
-
 
 	return (num);
-
 }
+
+
+ 
+
